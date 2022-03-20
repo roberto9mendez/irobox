a0ef7415fca21d990cbc3dca429ece9a53c74b6e
roberto9mendez <roberto9mendez009@gmail.com>
2022-03-21 00:10:39 +0400

    second

---
+++ script.js
@@ -0,0 +1,35 @@
+
+      function perguntaOnFn() {
+        document.getElementById("pergunta").style.display = "block";
+      }
+      function perguntaOffFn() {
+        document.getElementById("pergunta").style.display = "none";
+      }
+      
+      function androidOnFn() {
+        document.getElementById("android").style.display = "block";
+      }
+      function androidOffFn() {
+        document.getElementById("android").style.display = "none";
+      }
+      
+      function conteudoOnFn() {
+        document.getElementById("conteudo").style.display = "block";
+      }
+      function conteudoOffFn() {
+        document.getElementById("conteudo").style.display = "none";
+      }
+      
+      function resultadoOnFn() {
+        document.getElementById("resultado").style.display = "block";
+      }
+      function resultadoOffFn() {
+        document.getElementById("resultado").style.display = "none";
+      }
+      
+      function trabalhoOnFn() {
+        document.getElementById("trabalho").style.display = "block";
+      }
+      function trabalhoOffFn() {
+        document.getElementById("trabalho").style.display = "none";
+      }
